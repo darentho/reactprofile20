758a32901797e95fe3c1cdb5b6a32a8ed13cf4c6
Alberto Monroy <firewind3882@gmail.com>
2022-11-10 14:29:36 -0600

    started the banner component

---
+++ client/src/components/Banner.js
@@ -0,0 +1,11 @@
+export const Banner = () => {
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12}></Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
