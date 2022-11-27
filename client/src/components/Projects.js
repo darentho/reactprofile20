2b1cffbc05f4985f0720393701934c367f985867
Alberto Monroy <firewind3882@gmail.com>
2022-11-26 20:55:33 -0600

    added final project pictures for display

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,8 +1,8 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.jpg";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
+import projImg1 from "../assets/img/proj-img1.jpg";
+import projImg2 from "../assets/img/proj-img2.jpg";
+import projImg3 from "../assets/img/proj-img3.jpg";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
